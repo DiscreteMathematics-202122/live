50f8a2b5da8b9cb83d546878dce59fee5ea9cd60
kmurphy <kmurphy@wit.ie>
2021-09-21 22:28:17 +0100

    setup_magick

---
+++ resources/setup_practical_magic.py
@@ -0,0 +1,25 @@
+__required_version__ = 2020.1
+
+# check if we need to install / update 
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import os.path
+if os.path.isdir("setup_practical_magic"):
+    from setup_practical_magic import __version__
+else:
+    __version__ = 2019.1
+if __version__<__required_version__ or True:
+    print('Loading ... ')
+    import urllib.request
+    content = urllib.request.urlopen ("http://http://Discrete_Mathematics-202122.github.io/live/resources/setup_practical_magic.zip")
+    import zipfile
+    from io import BytesIO
+    data = zipfile.ZipFile(BytesIO(content.read()))
+    data.extractall(".")
+    
+# now import magicx  
+from setup_practical_magic import *
+
+
